1068871b5dbd3f42f8639f30ef4002b822f210e4
b3m3 <b3m3@yandex.ru>
2022-07-01 23:34:55 +0300

    create team-info.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import burger from './modules/burger.js';
 import parallax from './modules/parallax.js';
 import menuEvents from './modules/menu-events.js';
 import slider from './modules/slider.js';
+import teamInfo from './modules/team-info.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -10,4 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
   parallax();
   menuEvents();
   slider();
+  teamInfo();
 });
