3e75080c823fa2d46fcec245e222898e7c06aa04
b3m3 <b3m3@yandex.ru>
2022-07-02 11:30:12 +0300

    fix / team-page style / block >hover-team-page< / team-info.js

--- src/js/modules/team-info.js
+++ src/js/modules/team-info.js
@@ -1,23 +1,25 @@
 const teamInfo = () => {
-  const wrapp = document.querySelector('.team-page');
+  const section = document.querySelector('.team-page');
   const items = document.querySelectorAll('.drs-team-page__img');
   const info = document.querySelector('.hover-team-page');
 
-  items.forEach(item => {
-    item.addEventListener('mousemove', (e) => {
-      info.classList.add('show-info');
+  if (section) {
+    
+    items.forEach(item => {
+      item.addEventListener('mousemove', (e) => {
+        info.classList.add('show-info');
+      });
     });
-  });
-
-  wrapp.addEventListener('mousemove', (e) => {
-    console.log(e.target);
-    if (!e.target.classList.contains('sw-i') && 
-        !e.target.closest('.hover-team-page') &&
-        !e.target.classList.contains('hover-team-page')
-    ) {
-      info.classList.remove('show-info');
-    }
-  });
+  
+    section.addEventListener('mousemove', (e) => {
+      if (!e.target.classList.contains('sw-i') && 
+          !e.target.closest('.hover-team-page') &&
+          !e.target.classList.contains('hover-team-page')
+      ) {
+        info.classList.remove('show-info');
+      }
+    });
+  }
 };
 
 export default teamInfo;
