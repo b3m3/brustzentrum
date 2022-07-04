fc3f16127c5d990db2ead45f00df1d1247dee332
b3m3 <b3m3@yandex.ru>
2022-07-04 13:55:47 +0300

    photo pointer

--- src/js/modules/team-info.js
+++ src/js/modules/team-info.js
@@ -4,26 +4,31 @@ const teamInfo = () => {
   const info = document.querySelector('.hover-team-page');
   const pointers = document.querySelectorAll('.drs-team-page__p');
 
-  // if (section) {
-    
-  //   items.forEach((item, i) => {
-  //     item.addEventListener('mousemove', (e) => {
-  //       info.classList.add('show-info');
-  //       pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
-  //       pointers[i].classList.add('show-pointer');
-  //     });
-  //   });
+  if (section) {
+
+    const showInfo = (event) => {
+      items.forEach((item, i) => {
+        item.addEventListener(event, () => {
+          info.classList.add('show-info');
+          pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+          pointers[i].classList.add('show-pointer');
+        });
+      });
+    };
+
+    showInfo('click');
+    showInfo('mousemove');
   
-  //   section.addEventListener('mousemove', (e) => {
-  //     if (!e.target.classList.contains('sw-i') && 
-  //         !e.target.closest('.hover-team-page') &&
-  //         !e.target.classList.contains('hover-team-page')
-  //     ) {
-  //       info.classList.remove('show-info');
-  //       pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
-  //     }
-  //   });
-  // }
+    section.addEventListener('mousemove', (e) => {
+      if (!e.target.classList.contains('sw-i') && 
+          !e.target.closest('.hover-team-page') &&
+          !e.target.classList.contains('hover-team-page')
+      ) {
+        info.classList.remove('show-info');
+        pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+      }
+    });
+  }
 };
 
 export default teamInfo;
