3735c7ae3cb7222cefc6edd482b78a55c91cb459
b3m3 <b3m3@yandex.ru>
2022-07-04 12:16:29 +0300

    Termine > Termin / set mail in buttons

--- src/js/modules/team-info.js
+++ src/js/modules/team-info.js
@@ -2,24 +2,28 @@ const teamInfo = () => {
   const section = document.querySelector('.team-page');
   const items = document.querySelectorAll('.drs-team-page__img');
   const info = document.querySelector('.hover-team-page');
+  const pointers = document.querySelectorAll('.drs-team-page__p');
 
-  if (section) {
+  // if (section) {
     
-    items.forEach((item, i) => {
-      item.addEventListener('mousemove', (e) => {
-        info.classList.add('show-info');
-      });
-    });
+  //   items.forEach((item, i) => {
+  //     item.addEventListener('mousemove', (e) => {
+  //       info.classList.add('show-info');
+  //       pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+  //       pointers[i].classList.add('show-pointer');
+  //     });
+  //   });
   
-    section.addEventListener('mousemove', (e) => {
-      if (!e.target.classList.contains('sw-i') && 
-          !e.target.closest('.hover-team-page') &&
-          !e.target.classList.contains('hover-team-page')
-      ) {
-        info.classList.remove('show-info');
-      }
-    });
-  }
+  //   section.addEventListener('mousemove', (e) => {
+  //     if (!e.target.classList.contains('sw-i') && 
+  //         !e.target.closest('.hover-team-page') &&
+  //         !e.target.classList.contains('hover-team-page')
+  //     ) {
+  //       info.classList.remove('show-info');
+  //       pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+  //     }
+  //   });
+  // }
 };
 
 export default teamInfo;
