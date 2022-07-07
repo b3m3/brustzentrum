f020b20fa3255842a0b992f6551acbb2918ed269
b3m3 <b3m3@yandex.ru>
2022-07-07 14:59:51 +0300

    fix decore / edit team-info.js / edit style vision image blur

--- src/js/modules/parallax.js
+++ src/js/modules/parallax.js
@@ -3,27 +3,36 @@ const parallax = () => {
   const background = document.querySelector('.team__bg');
   const clientHeight = document.documentElement.clientHeight;
   
-  const distance = () => 
-    window.scrollY / 8 - 200 < 55 ? window.scrollY / 7 - 200 : null;
+  // const distance = () => {
+  //   if (window.scrollY / 8 - 200) {
+  //     return window.scrollY / 7 - 200;
+  //   }
+  // };
 
-  const scroll = () => 
-    window.scrollY >= section.offsetTop - clientHeight ? 
-    background.style.transform = `translateY(-${distance()}px)` :
-    null;
+  const scroll = () => {
+
+  };
 
   const checkSize = width => {
-
     if (document.documentElement.clientWidth > width) {
-      window.addEventListener('scroll', scroll);
-    } else {
-      window.removeEventListener('scroll', scroll);
-      background.style.transform = `translateY(${0}px)`;
+      window.addEventListener('scroll', () => {
+        // scroll();
+        if (window.scrollY + section.clientHeight - clientHeight >= section.offsetTop) {
+          background.style.transform = `translateY(-${window.scrollY / 100}px)`;
+          console.log('px', window.scrollY / 50);
+        }
+        else {
+          background.style.transform = `translateY(${0}px)`;
+        }
+      });
     }
   };
 
   if (section) {
-    checkSize(550);
-    window.addEventListener('resize', () => checkSize(550));
+
+    // checkSize(550);
+
+    // window.addEventListener('resize', () => checkSize(550));
   }
 };
 
--- src/js/modules/team-info.js
+++ src/js/modules/team-info.js
@@ -6,21 +6,39 @@ const teamInfo = () => {
 
   if (section) {
 
+    // const showInfo = (event) => {
+    //   items.forEach((item, i) => {
+    //     item.addEventListener(event, () => {
+    //       info.classList.add('show-info');
+    //       pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+    //       pointers[i].classList.add('show-pointer');
+    //     });
+    //   });
+    // };
+
     const showInfo = (event) => {
-      items.forEach((item, i) => {
-        item.addEventListener(event, () => {
-          info.classList.add('show-info');
-          pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
-          pointers[i].classList.add('show-pointer');
-        });
+      items[0].addEventListener(event, () => {
+        info.classList.add('show-info');
+        pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+        pointers[0].classList.add('show-pointer');
       });
     };
 
     showInfo('click');
     showInfo('mousemove');
   
+    // section.addEventListener('mousemove', (e) => {
+    //   if (!e.target.classList.contains('sw-i') && 
+    //       !e.target.closest('.hover-team-page') &&
+    //       !e.target.classList.contains('hover-team-page')
+    //   ) {
+    //     info.classList.remove('show-info');
+    //     pointers.forEach(pointer => pointer.classList.remove('show-pointer'));
+    //   }
+    // });
+
     section.addEventListener('mousemove', (e) => {
-      if (!e.target.classList.contains('sw-i') && 
+      if (!e.target.classList.contains('sw-i-f') && 
           !e.target.closest('.hover-team-page') &&
           !e.target.classList.contains('hover-team-page')
       ) {
